d8a05681e75e139dd39f92a3c49dc49c40cd4826
KentOJL <102277758+KentOJL@users.noreply.github.com>
2022-03-25 17:47:45 +0800

    Update Sales_mangament.py

--- Sales_mangament.py
+++ Sales_mangament.py
@@ -11,49 +11,55 @@ all_products = [
 ]
 
 def banner():
-    print("_" * 40)
-    print("Welcome to Razer Mouse Shop")
-    print("_" * 40)
+    print("_" * 60)
+    print("\nWelcome to Razer Mouse Shop")
+    print("_" * 60)
     print("""\n1. Show All Products
             \n2. Sales
             \n3. Add Products
             \n4. Remove Products
             \n5. Exit""")
-    print("_" * 40)
+    print("_" * 60)
 
 
 
 def display_all():
     print("SNO\tProduct\t\t\t\tIn Stock\tPrice")
     for item in all_products:
-        print("{0}\t{1}\t\t{2}\t\t{3}".format(item[0], item[1], item[2], item[3]))
+        print("\n{0}\t{1}\t\t{2}\t\t{3:.2f}".format(item[0], item[1], item[2], item[3]))
 
 
 
-def order_summary(product, price):
-    print("-" * 40)
+def order_summary(product, price , total , q_lst):
+    print("-" * 60)
     print("\t\tRazer Mouse Shop")
-    print("-" * 40)
-    print("Order Summary\tDate:{}".format(str(datetime.now())))
-    for i in range(len(product)):
-        print("Product Name: {}".format(i))
-    print("Price: {}".format(price))
-    print("-" * 40)
-    print("\t\tTotal Payment Amount: {}".format(item[-1]))
+    print("-" * 60)
+    print("Order Summary\t\tDate:{}".format(str(datetime.now())))
+    print(" ")
+    print("Product name\t\t\tQuantity\tPrice")
+    print("-" * 60)
+    for j in range(len(product)):
+        print("{}\t\t  {}\t\tRM {:.2f}".format(product[j] , q_lst[j] ,price[j] ))
+    print("-" * 60)
+    print("Total Payment Amount:\t\t\t\tRM {:.2f}".format(total))
 
 
 
-def generate_bill(product, price, lst):
-    print("-" * 40)
-    print("\t\tRazer Mouse Shop")
-    print("-" * 40)
-    print("Bill:{} \tDate:{}".format(int(random.random()*100000), str(datetime.now())))
-    print("Product name\t\tprice")
-    print("-" * 40)
+def generate_bill(product, total, lst , price , q_lst , Change , amt_received):
+    print("-" * 60)
+    print("\n\tRazer Mouse Shop")
+    print("-" * 60)
+    print("Bill:{} \t\tDate:{}".format(int(random.random()*100000), str(datetime.now())))
+    print(" ")
+    print("Product name\t\t\tQuantity\tPrice")
+    print("-" * 60)
     for j in range(len(lst)):
-        print("{}\t\tRM{}".format(j , item[-1]))
-    print("-" * 40)
-    print("\t\tTotal Bill Amount: {}".format(price))
+        print("{}\t\t  {}\t\tRM {:.2f}".format(lst[j] , q_lst[j] ,price[j] ))
+    print("-" * 60)
+    print("Total Bill Amount:\t\t\t\tRM {:.2f}".format(total))
+    print("  Amount Received:\t\t\t\tRM {:.2f}".format(amt_received))
+    print("           Change:\t\t\t\tRM {:.2f}".format(Change))
+
 
 
 
@@ -66,27 +72,47 @@ while(True):
         display_all()
         print("Press 0 for payment")
         item_lst = []
+        price_lst = []
+        quantity_lst = []
         total_price = 0
-        prod_id = int(input("Enter the Product ID: "))
+        prod_id = 999
         while prod_id != 0:
             prod_id = int(input("Enter the Product ID: "))
             for item in all_products:
                 if item[0] == prod_id:
-                    item_lst.append(item[0])
-                    total_price = total_price + item[2]
-                    if item[1] == 0:
+                    quantity = int(input("Please enter the quantity: "))
+                    item_lst.append(item[1])
+                    quantity_lst.append(quantity)
+                    price_q = item[3] * quantity
+                    price_lst.append(price_q)
+                    total_price = total_price + price_q
+                    if item[2] == 0:
                         print("Sorry we are out of stock")
                     else:
-                        item[1] -= 1
-                    
-                member = input("Do you have membership(Y/N): ")
-                if member == "Y":
-                    total_price * 0.9
-                    generate_bill(item, total_price, item_lst)
-                    print("Thanks For shopping with Us")
-                    sys.exit(0)
-                else:
-                    print("Continue Shopping")
+                        item[2] -= 1
+        order_summary(item_lst , price_lst , total_price , quantity_lst)
+        print(" ")
+        conf = input("Please confirm your order(Y/N): ")
+        if conf == "Y":
+            member = input("Do you have membership(Y/N): ")
+            if member == "Y":
+                total_price = total_price * 0.9
+                payment = float(input("Amount received: "))
+                change = payment - total_price
+                generate_bill(item, total_price, item_lst , price_lst , quantity_lst ,change , payment)
+                print(" ")
+                print("Thanks For shopping with Us :)")
+                sys.exit(0)
+            else:
+                payment = float(input("Amount received: "))
+                change = payment - total_price
+                generate_bill(item, total_price, item_lst , price_lst , quantity_lst ,change , payment)
+                print(" ")
+                print("Thanks For shopping with Us :)")
+                sys.exit(0)
+        else:
+            print("Continue Exploring the shop")
+    
     elif choice == 3:
         username = input("Enter Admin UserID: ")
         password = input("Enter the Password: ")
@@ -95,7 +121,7 @@ while(True):
             prod.append(len(all_products)+1)
             prod.append(input("Enter the Product Name: "))
             prod.append(int(input("Quantity: ")))
-            prod.append(int(input("Price: ")))
+            prod.append(float(input("Price: ")))
             all_products.append(prod)
         else:
             print("Incorrect username and password")
